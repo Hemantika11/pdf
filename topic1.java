ea4a40edca100e80d016b937bd8c3f502ccf3cc4
Hemantika <hemarpatel@yahoo.co.uk>
2022-12-06 13:43:24 +0000

    Javamodule

---
+++ topic1.java
@@ -0,0 +1,16 @@
+package pdf3;
+
+public class topic1
+{
+        public static void main(String args[])
+        {
+
+            System.out.println("Software Development Models");
+            System.out.println("Waterfall");
+            System.out.println("Iterative");
+            System.out.println("V-Model");
+            System.out.println("Verification and Validation");
+            System.out.println("Agile");
+            System.out.println("Definiton of Redy/Done");
+        }
+}
---
+++ topic2.java
@@ -0,0 +1,13 @@
+package pdf3;
+
+public class topic2
+{
+    public static void main(String args[])
+    {
+        System.out.println("What is TDD");
+        System.out.println("What is BDD");
+        System.out.println("BDD keywords");
+        System.out.println("How to write test cases with BDD");
+        System.out.println("Maintenance");
+    }
+}
